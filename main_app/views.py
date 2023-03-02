e5c2fed3582c1bfa62d183d0bf1189b91d09e946
AlexMcBex <alessiobelli94@gmail.com>
2023-03-02 14:30:39 -0500

    cats index shows cats from sql database

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Cat
+
 #temporary cats for building templates
 cats = [
   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
@@ -16,4 +18,5 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', { 'cats': cats })
