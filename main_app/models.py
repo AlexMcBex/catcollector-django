6b5ed8afe51cfe8032d9b5208a1694cfc15573f6
AlexMcBex <alessiobelli94@gmail.com>
2023-03-03 14:17:43 -0500

    add feeding model and show them in detail

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+# A tuple of 2-tuples
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 
 #class inheritance
@@ -15,3 +22,16 @@ class Cat(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'cat_id' : self.id})
+    
+# Add new Feeding model below Cat model
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        choices= MEALS,
+        default= MEALS[0][0],
+        max_length=1
+    )
+    #add cat foreign key reference
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.get_meal_display()} on {self.date}'
