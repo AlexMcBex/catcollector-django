48ee405405bf7e36d080f6ba632bd248beed0a69
AlexMcBex <alessiobelli94@gmail.com>
2023-03-03 16:47:15 -0500

    added toys, user can add or remove toys from cats

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # A tuple of 2-tuples
 MEALS = (
@@ -9,13 +10,25 @@ MEALS = (
 )
 
 # Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f'{self.color} {self.name}'
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
 #class inheritance
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
     def __str__(self):
         return self.name
@@ -35,3 +48,6 @@ class Feeding(models.Model):
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
     def __str__(self):
         return f'{self.get_meal_display()} on {self.date}'
+    
+class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Cat
+from .models import Cat, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from .forms import FeedingForm
 
 #temporary cats for building templates
@@ -25,8 +27,10 @@ def cats_index(request):
 
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
+  id_list = cat.toys.all().values_list('id')
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in=id_list)
   feeding_form = FeedingForm()
-  return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form })
+  return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form , 'toys': toys_cat_doesnt_have})
 
   
 
@@ -52,3 +56,43 @@ def add_feeding(request, cat_id):
     new_feeding.cat_id = cat_id
     new_feeding.save()
   return redirect('detail', cat_id=cat_id)
+
+# ToyList
+class ToyList(ListView):
+  model = Toy
+  template_name = 'toys/index.html'
+
+  # def get_query(self):
+
+def assoc_toy(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.add(toy_id)
+  return redirect('detail', cat_id=cat_id)
+
+def unassoc_toy(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.remove(toy_id)
+  return redirect('detail', cat_id=cat_id)
+
+# ToyDetail
+class ToyDetail(DetailView):
+  model = Toy
+  template_name = 'toys/detail.html'
+
+# ToyCreate
+class ToyCreate(CreateView):
+  model = Toy
+  fields = ['name', 'color']
+  
+  def form_valid(self, form):
+    return super().form_valid(form)
+
+
+# ToyUpdate
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+
+# ToyDelete
+class ToyDelete(DeleteView):
+  model= Toy
+  success_url = '/toys/'
