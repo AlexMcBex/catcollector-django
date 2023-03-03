a11f06612e147630ebc838a80b6f5f4b470ba61f
AlexMcBex <alessiobelli94@gmail.com>
2023-03-03 14:52:08 -0500

    users can add feedings to the cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cat
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
 
 #temporary cats for building templates
 # cats = [
@@ -24,7 +25,10 @@ def cats_index(request):
 
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', { 'cat': cat })
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', { 'cat': cat, 'feeding_form': feeding_form })
+
+  
 
 class CatCreate(CreateView):
   model = Cat
@@ -40,3 +44,11 @@ class CatUpdate(UpdateView):
 class CatDelete(DeleteView):
   model = Cat
   success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit = False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
